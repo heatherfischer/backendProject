19f27c206819723b58ecb98f0a6eec4a22f06c92
Heather Fischer <heather.fischer6@gmail.com>
2022-01-03 15:02:47 +0100

    server set up and connected to MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require ("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const port = 3001;
 
+dotenv.config();
 app.use(express.json());
 app.use(cors());
+
+const {URL} = process.env;
+
+mongoose.connect(URL)
+    .then(() => {console.log('Connected to the database')})
+    .catch((error) => {console.log('Not connected to the database -error occurred', error)})
+
+app.listen(port, () => {
+    console.log(`The server 🙈 is listening on port ${port}`);
+});

0ac0286db929e46be5c5c5299a49d8447da16405
Heather Fischer <heather.fischer6@gmail.com>
2022-01-05 15:10:00 +0100

    create controllers to add contact, find and updated contacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,3 +1,4 @@
+const reactDom = require('react-dom');
 const Contact = require('../models/contact');
 
 exports.createContact = async (req, res) => {
@@ -5,20 +6,7 @@ exports.createContact = async (req, res) => {
     try {
         const newContact= await Contact.create(
             {
-                firstName: body.firstName,
-                lastName: body.lastName,
-                company:  body.company,
-                customerType: body.customerType, 
-                addressStreet: body.addressStreet,
-                addressCity: body.addressCity,
-                addressPostcode: body.addressPostcode,
-                phoneNumber: body.phoneNumber,
-                email: body.email,
-                salesFunnelStage: body.salesFunnelStage,
-                maxEnergyGenPotentialkW: body.maxEnergyGenPotentialkW,
-                peakEnergyDemandkW: body.peakEnergyDemandkW,
-                gridOperator: body.gridOperator,
-                salesNotes: body.salesNotes,
+                ...body
             });
         return res.status(200).json({
             message:'Contact was created', createdContact: newContact
@@ -29,3 +17,76 @@ exports.createContact = async (req, res) => {
     }
 
 }
+
+exports.listAllContacts = async (req, res) => {
+    try {
+        const contactList = await Contact.find();
+
+        return res.status(200).json(contactList);
+    } catch (error) {
+        return res.status(400).json({message:'error!', error:error});
+    }
+}
+
+exports.listAllPrivate = async (req, res) => {
+    try {
+        const privateContacts = await Contact.find({"customerType": "private"});
+        return res.status(200).json(privateContacts);
+    } catch (error) {
+        return res.status(400).json({message:'error!', error:error})
+    }
+}
+
+exports.listAllCommercial = async (req, res) => {
+    try {
+        const commercialContacts = await Contact.find({"customerType": "commercial"});
+        return res.status(200).json(commercialContacts);
+    } catch (error) {
+        return res.status(400).json({message:'error!', error:error})
+    }
+}
+
+exports.updateContact = async (req, res) => {
+    try {
+        const contact = await Contact.findOneAndUpdate(req.params.id,{
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            company: req.body.company,
+            customerType: req.body.customerType, 
+            addressStreet: req.body.addressStreet,
+            addressCity: req.body.addressCity,
+            addressPostcode: req.body.addressPostcode,
+            phoneNumber: req.body.phoneNumber,
+            email: req.body.email,
+            salesFunnelStage: req.body.salesFunnelStage,
+            maxEnergyGenPotentialkW: req.body.maxEnergyGenPotentialkW,
+            energyDemandkWh: req.body.energyDemandkWh,
+            gridOperator: req.body.gridOperator,
+            salesNotes: req.body.salesNotes
+        }, {new:true})
+        if (!contact) {
+            return res.status(404).json("contact not found")
+        }
+
+        return res.status(200).json({updatedContact:contact})
+    } catch (error) {
+        return res.status(400).json({message:'error!', error:error})
+    }
+
+}
+
+
+
+
+//attempt to make a blanket search route - needs regex, will look at later
+// exports.search = async (req, res) => {
+//     const {params}=req;
+//     try {
+//         const customers = Contact.find(params);
+//         return res.status(200).json(customers);
+//     } catch (error) {
+//         return res.status(400).json({message:'error', error:error})
+//     }
+// }
+
+
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,5 +4,10 @@ const controller = require('../controllers/contactController');
 const router = express.Router();
 
 router.post('/add', controller.createContact);
+router.get('/list', controller.listAllContacts);
+router.get('/private', controller.listAllPrivate);
+router.get('/commercial', controller.listAllCommercial);
+// router.get('/search', controller.search);
+router.patch('/update', controller.updateContact);
 
 module.exports = router;
